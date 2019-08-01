f41d75be29a0a1c63035148d6a726095359a1a19
Kurtis Raymond <kraymond@sfu.ca>
2019-08-01 12:21:15 -0700

    changed form of fipps parser

--- include/TLstEvent.h
+++ include/TLstEvent.h
@@ -37,13 +37,15 @@ public:
    // helpers for event creation
 
    char* GetData() override; ///< return pointer to the data buffer
-
    void SetData(std::vector<char>& buffer); ///< set an externally allocated data buffer
+   void SetLstVersion(int8_t version) { fLstVersion = version };
+   int8_t GetLstVersion() { return fLstVersion };
 
    int SwapBytes(bool) override; ///< convert event data between little-endian (Linux-x86) and big endian (MacOS-PPC)
 
 protected:
    std::vector<char> fData; ///< event data buffer
+   int8_t fLstVersion; ///< Lst version
 
    /// \cond CLASSIMP
    ClassDefOverride(TLstEvent, 0) // All of the data contained in a Midas Event
--- libraries/TILLDataParser/TILLDataParser.cxx
+++ libraries/TILLDataParser/TILLDataParser.cxx
@@ -30,80 +30,102 @@ int TILLDataParser::Process(std::shared_ptr<TRawEvent> rawEvent)
    /// Returns the total number of fragments read (good and bad).
    // right now the parser only returns the total number of fragments read
    // so we assume (for now) that all fragments are good fragments
+   int EventsProcessed = 0;
 	std::shared_ptr<TLstEvent> event = std::static_pointer_cast<TLstEvent>(rawEvent);
-   return FippsToFragment(event->GetData(), event->GetDataSize());
+   if( fInputSize != nullptr )
+      *fInputSize += event->GetDataSize()/16 // 16 bytes per event
+   if( event->GetLstVersion() == 1 ) {
+      for( size_t i = 0; i + 3 < size / 4; i += 4 ) {
+         EventsProcessed += V1SingleFippsEventToFragment(event->GetData() + i);
+         if(fItemsPopped != nullptr && fInputSize != nullptr) {
+            ++(*fItemsPopped);
+            --(*fInputSize);
+         }
+      }
+   } else {
+      for( size_t i = 0; i + 3 < size / 4; i += 4 ) {
+         EventsProcessed += V2SingleFippsEventToFragment(event->GetData() + i);
+         if(fItemsPopped != nullptr && fInputSize != nullptr) {
+            ++(*fItemsPopped);
+            --(*fInputSize);
+         }
+      }
+   }
+
+   return EventsProcessed;
 }
 
-int TILLDataParser::FippsToFragment(char* data, uint32_t size)
+int TILLDataParser::V1SingleFippsEventToFragment(char* data)
 {
    uint32_t* ptr = reinterpret_cast<uint32_t*>(data);
-
-   int                        totalEventsRead = 0;
    std::shared_ptr<TFragment> eventFrag       = std::make_shared<TFragment>();
    Long64_t                   tmpTimestamp;
-   if(fItemsPopped != nullptr && fInputSize != nullptr) {
-      *fItemsPopped += 0;
-      *fInputSize   += size/16; //4 words of 4 bytes for each event
+
+   eventFrag->SetAddress( (ptr[0] >> 16) & 0xffff );
+
+   // Rollover, constant beween boards
+   tmpTimestamp = ptr[0] & 0xffff;
+
+   // Concatonate timestamp informatioan based on board type
+   switch( static_cast<EDigitizer>((ptr[0] >> 22) && 0x3F) ) {
+      case EDigitizer::kV1724: 
+         tmpTimestamp = tmpTimestamp<<30;
+         tmpTimestamp |= ptr[1] & 0x3ffffffF; // 30 bit timestamp
+      case EDigitizer::kV1725:
+         tmpTimestamp = tmpTimestamp<<31;
+         tmpTimestamp |= ptr[1] & 0x7ffffffF; // 31 bit timestamp
+      default:
+         tmpTimestamp = tmpTimestamp<<32;
+         tmpTimestamp |= ptr[1] & 0xffffffff; // 32 bit timestamp
    }
+   eventFrag->SetTimeStamp(tmpTimestamp);
 
-   // *=* LST File Parsing *=* //
-   // This section is for reading in V1 .lst files of a C1725 board
-   // TODO: Change this to read in both V1 and V2 type files, as well as all possible DAQ boards at ILL.
-   // This reqires reading the header at the beginning of the .lst file to find the version.
-
-   // we read 4 words for each event, and size is in bytes, so we need to divide it by 4 (size of uint32_t)
-   for(size_t i = 0; i + 3 < size / 4; i += 4) {
-      if(fItemsPopped != nullptr && fInputSize != nullptr) {
-         ++(*fItemsPopped);
-         --(*fInputSize);
-      }
-
-      // Crate: (buffer >> 28) & 0xF (4 Bits)
-      // Board: (buffer >>  22) & 0x3F (6 Bits)
-      EDigitizer Board = static_cast<EDigitizer>(( ptr[i] >> 22) & 0x3F);
-      
-      // Channel: (Buffer >> 16) & 0x3F (6 Bits)
-      eventFrag->SetAddress( (ptr[i] >> 16) & 0xffff );
-
-      //*=* Timestamp bits *=*//
-
-      // Rollover, constant beween boards
-      tmpTimestamp = ptr[i] & 0xffff;
-      tmpTimestamp = tmpTimestamp<<31;
-
-      // Concatonate timestamp informatioan
-      if( Board == EDigitizer::kV1724 ) {
-          // Timestamp bits is 30
-          tmpTimestamp |= ptr[ i + 1 ] & 0x3FFFFFFF;
-      } else if ( Board == EDigitizer::kV1725_PHA ) {
-          // Timestamp bits is 31
-          tmpTimestamp |= ptr[i + 1] & 0x7fffffff;
-      } else {
-          tmpTimestamp |= ptr[i + 1] & 0xffffffff;
-      }
-      eventFrag->SetTimeStamp(tmpTimestamp);
-
-      ++totalEventsRead;
-
-      //*=* Charge Information *=*//
-      int32_t Charge = (ptr[i+2] & 0x7FFF);
-
-      // Discriminate bad fragments
-      if( Charge == 0 || Charge == 0x8000 ) {
-         if(fRecordDiag) {
-            TParsingDiagnostics::Get()->BadFragment(99);
-         }
-         // Push(*fBadOutputQueue, std::make_shared<TBadFragment>(*eventFrag, ptr, size / 4, i + 2, false));
-         continue;
-      }
-      eventFrag->SetCharge(static_cast<int32_t>(Charge) );
+   int32_t Charge = (ptr[2] & 0x7fff);
+   // Discriminate bad fragments
+   if( Charge == 0 || Charge == 0x8000 ) {
       if(fRecordDiag) {
-         TParsingDiagnostics::Get()->GoodFragment(eventFrag);
+         TParsingDiagnostics::Get()->BadFragment(99);
       }
-      Push(fGoodOutputQueues, std::make_shared<TFragment>(*eventFrag));
-      // std::cout<<totalEventsRead<<": "<<eventFrag->Charge()<<", "<<eventFrag->GetTimeStamp()<<std::endl;
+      Push(*fBadOutputQueue, std::make_shared<TBadFragment>(*eventFrag, ptr, 4, 2, false));
+      return 0;
    }
-
-   return totalEventsRead;
+   // Good event
+   eventFrag->SetCharge(static_cast<int32_t>(Charge) );
+   if(fRecordDiag) {
+      TParsingDiagnostics::Get()->GoodFragment(eventFrag);
+   }
+   Push(fGoodOutputQueues, std::make_shared<TFragment>(*eventFrag));
+   return 1;
 }
 
+int TILLDataParser::V2SingleFippsEventToFragment(char* data)
+{
+   uint32_t* ptr = reinterpret_cast<uint32_t*>(data);
+   std::shared_ptr<TFragment> eventFrag       = std::make_shared<TFragment>();
+   Long64_t                   tmpTimestamp;
+
+   eventFrag->SetAddress( (ptr[0] >> 16) & 0xffff );
+
+   // Rollover, constant beween boards
+   tmpTimestamp = ptr[0] & 0xffff;
+   tmpTimestamp = tmpTimestamp<<32;
+   tmpTimestamp |= ptr[1] & 0xffffffff; // 32 bit timestamp
+   eventFrag->SetTimeStamp(tmpTimestamp);
+
+   int32_t Charge = (ptr[2] & 0x7fff);
+   // Discriminate bad fragments
+   if( Charge == 0 || Charge == 0x8000 ) {
+      if(fRecordDiag) {
+         TParsingDiagnostics::Get()->BadFragment(99);
+      }
+      Push(*fBadOutputQueue, std::make_shared<TBadFragment>(*eventFrag, ptr, 4, 2, false));
+      return 0;
+   }
+   // Good event
+   eventFrag->SetCharge(static_cast<int32_t>(Charge) );
+   if(fRecordDiag) {
+      TParsingDiagnostics::Get()->GoodFragment(eventFrag);
+   }
+   Push(fGoodOutputQueues, std::make_shared<TFragment>(*eventFrag));
+   return 1;
+}
